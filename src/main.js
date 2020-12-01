2d2507ba4aca97c47cd278ad27020585a45ee48f
pareshvarik <varikparesh97@gmail.com>
2020-12-01 17:05:09 +0530

    added configuration for  vuex store

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import Vuex from "vuex";
+import storeIndex from "./store/index";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+Vue.use(Vuex);
 // Install BootstrapVue
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
+//configuration for store
+export const store = new Vuex.Store(
+  storeIndex
+);
 
 Vue.config.productionTip = false;
 
